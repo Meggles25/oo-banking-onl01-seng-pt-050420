429c083d11e515962a3a43258435a4527848163c
Megan Gomes <megan.gomes25@gmail.com>
2020-06-11 01:09:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,14 +26,9 @@ class Transfer
     
   def reverse_transfer
     if @status == "complete"
-      @sender.
-    
-    
-    
-  
-
-
-
-
-
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end  
 end
